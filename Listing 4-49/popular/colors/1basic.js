88549a59d7eb37e0b96da7d15ec1076c1f837d6d
sawmadala1 <sawmadala1@southernct.edu>
2015-09-24 21:44:45 +0000

    CSC443 homework-update

---
+++ Listing 4-49/popular/colors/1basic.js
@@ -0,0 +1,8 @@
+// Loading this module modifies string for the entire process
+require('colors');
+
+console.log('hello'.green);
+console.log('world!'.red);
+console.log('Feeling yellow'.yellow);
+console.log('But you look blue'.blue);
+console.log('This should cheer you up!'.rainbow);
